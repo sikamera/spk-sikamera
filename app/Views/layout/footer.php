e03406426e0c192b1155b0682b01e30b75f55d1d
AdmajaRois <rois.da.silva@gmail.com>
2020-10-12 23:32:38 +0700

    adding navbar, sidebar, footer

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,18 @@
+<?= $this->extend('layout/page_layout'); ?>
+
+
+
+<?= $this->section('content'); ?>
+
+ <!-- Footer -->
+    <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2020</span>
+          </div>
+        </div>
+    </footer>
+      <!-- End of Footer -->
+
+
+<?= $this->endSection(); ?>
--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -3,9 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>title</title>
+    <link href="theme/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="theme/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
-<body>
+<body id="page-top"> 
+<div class="wrapper">
+    <?= $this->include('layout/sidebar'); ?>
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+            <?= $this->include('layout/navbar'); ?>
+            <div class="container-fluid">
+                <?= $this->renderSection('content'); ?>
+            </div>
+        </div>
+        <?= $this->include('layout/footer'); ?>
+    </div>
+</div>
+   
+
+   
+
+   
+
     
+    
+    <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
 </body>
 </html>
