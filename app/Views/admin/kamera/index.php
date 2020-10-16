619f66ec0749c77c550c8baac02b06f5341b66d4
AdmajaRois <rois.da.silva@gmail.com>
2020-10-16 18:46:39 +0700

    data kamera done

--- app/Views/admin/kamera/index.php
+++ app/Views/admin/kamera/index.php
@@ -2,6 +2,31 @@
 
 
 <?= $this->section('content'); ?>
-
+    <h1 class="text-center">Data Kamera</h1>
+    <div class="card mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><a href="kamera/tambah_kamera"><i class="fas fa-plus"></i> Tambah kamera</a></h6>
+        </div>
+        <div class="card-body">
+            <?php if($kamera != null): ?>
+            <div class="row">
+                <?php foreach($kamera as $kam): ?>
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card shadow h-100 py-2">
+                        <a href="">
+                            <img src="/img/kamera/<?= $kam['image']; ?>" alt="thumbnail" class="card-img-top" width="50px" height="100%">
+                        </a>
+                        <div class="card-body">
+                            <div class="card-title"><h3 class="text-center"><?= $kam['brand']." ".$kam['type']; ?></h3></div>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+            <?php else: ?>
+            <h1 class="text-center">Tidak ada data tersimpan!</h1>
+            <?php endif; ?>
+        </div>
+    </div>
 
 <?= $this->endSection(); ?>
