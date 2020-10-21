eaec9ed761a3811f9f6bf6f49aff3df51595a8b3
AdmajaRois <rois.da.silva@gmail.com>
2020-10-21 22:55:41 +0700

    ganti vendor tema menjadi assets

--- app/Views/admin/layout/template.php
+++ app/Views/admin/layout/template.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sikamera | <?= $judul; ?></title>
-    <link href="/theme/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/theme/assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="/theme/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="/theme/css/style.css" rel="stylesheet">
@@ -37,17 +37,17 @@
    
 
     <!-- Bootstrap core JavaScript-->
-  <script src="/theme/vendor/jquery/jquery.min.js"></script>
-  <script src="/theme/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/theme/assets/jquery/jquery.min.js"></script>
+  <script src="/theme/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="/theme/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="/theme/assets/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="/theme/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="theme/vendor/chart.js/Chart.min.js"></script>
+  <script src="theme/assets/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
   <script src="theme/js/demo/chart-area-demo.js"></script>
