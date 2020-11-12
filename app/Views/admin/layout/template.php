bcf30d2635c755f3e1fd85e7cf074c56f2b7ab9d
AdmajaRois <rois.da.silva@gmail.com>
2020-11-12 16:42:45 +0700

    add template for user

--- app/Views/admin/layout/template.php
+++ app/Views/admin/layout/template.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sikamera | <?= $judul; ?></title>
-    <link href="/theme/assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/theme/sbootstrap/assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="/theme/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="/theme/css/style.css" rel="stylesheet">
+    <link href="/theme/sbootstrap/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="/theme/sbootstrap/css/style.css" rel="stylesheet">
 </head>
 <body id="page-top"> 
 
@@ -33,21 +33,21 @@
    
 
     <!-- Bootstrap core JavaScript-->
-  <script src="/theme/assets/jquery/jquery.min.js"></script>
-  <script src="/theme/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/theme/sbootstrap/assets/jquery/jquery.min.js"></script>
+  <script src="/theme/sbootstrap/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="/theme/assets/jquery-easing/jquery.easing.min.js"></script>
+  <script src="/theme/sbootstrap/assets/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="/theme/js/sb-admin-2.min.js"></script>
+  <script src="/theme/sbootstrap/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="theme/assets/chart.js/Chart.min.js"></script>
+  <script src="theme/sbootstrap/assets/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="theme/js/demo/chart-area-demo.js"></script>
-  <script src="theme/js/demo/chart-pie-demo.js"></script>
+  <script src="theme/sbootstrap/js/demo/chart-area-demo.js"></script>
+  <script src="theme/sbootstrap/js/demo/chart-pie-demo.js"></script>
 
   <script>
       const preview = ()=>{
